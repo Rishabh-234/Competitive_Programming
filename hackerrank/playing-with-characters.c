356846698af0b90ea55b25daca8ead37ae3a4518
Rishabh Banthia <rishabhbanthia234@gmail.com>
2021-01-30 09:37:59 +0530

    Update playing-with-characters.c

--- hackerrank/playing-with-characters.c
+++ hackerrank/playing-with-characters.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-
 #define MAX_LEN 128
 
 int main() 
@@ -10,14 +9,11 @@ int main()
     char ch;
     char word[MAX_LEN];
     char sen[MAX_LEN];
-
     scanf("%c", &ch); 
     scanf("%s\n", &word);
     scanf("%[^\n]%*c", &sen);
-
     printf("%c\n", ch);  
     printf("%s\n", word); 
     printf("%s\n", sen); 
-
     return 0;
 }
